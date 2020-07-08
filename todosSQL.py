96b2b1f69dc6720856228a67b1ac999fdaa6a2a7
Filip Gabrys <el.filberto@gmail.com>
2020-07-08 20:47:12 +0200

    Update funkctions for sql

--- todosSQL.py
+++ todosSQL.py
@@ -12,11 +12,14 @@ def create_connection(db_file):
    conn = None
    try:
        conn = sqlite3.connect(db_file)
-       print(f"Connected to {db_file}")
        return conn
    except Error as e:
        print(e)
 
+   return conn
+
+    
+    
    
 
 # stworzenie nowej tabeli
@@ -33,24 +36,82 @@ def execute_sql(conn, sql):
        print(e)
 
 
-create_todo_sql = """
+if __name__ == "__main__":
+    
+
+    create_todo_sql = """
     -- todo table
     CREATE TABLE IF NOT EXISTS todo (
         id integer PRIMARY KEY,
         title text NOT NULL,
         description text,
-        done text
+        done text,
+        csrf_token text
     );
     """
-db_file = "todo.db"
 
-conn = create_connection(db_file)
-if conn is not None:
+    db_file = "todo.db"
+
+    conn = create_connection(db_file)
+    if conn is not None:
+        execute_sql(conn, create_todo_sql)
+        conn.close()
+
+
+
+
+
+class TodosSQL:
+    def __init__(self):
+        """
+        Query all rows in the table
+        :param conn: the Connection object
+        :return:
+        """
+        conn = create_connection("todo.db")
+        cur = conn.cursor()
+        cur.execute(f"SELECT * FROM todo")
+        rows = cur.fetchall()
+        #print(rows)
+        self.todos = rows
+
+    def all(self):
+        """
+        Query all rows in the table
+        :param conn: the Connection object
+        :return:
+        """
+        conn = create_connection("todo.db")
+        cur = conn.cursor()
+        cur.execute(f"SELECT * FROM todo")
+        rows = cur.fetchall()
+        return rows
+
+    def get(self, id):
+        return self.todos[id]
+
+    def create(self, data):
+        #data.pop('csrf_token')
+        """
+        Create a new projekt into the projects table
+        :param conn:
+        :param data:
+        :return: data id
+        """
+        conn = create_connection("todo.db")
+        sql = '''INSERT INTO todo(title, description, done, csrf_token)
+                VALUES(?,?,?,?)'''
+        cur = conn.cursor()
+        cur.execute(sql, data)
+        conn.commit()
+        return cur.lastrowid
+
+    def update(self, id, data):
+        data.pop('csrf_token')
+        self.todos[id] = data
         
-    execute_sql(conn, create_todo_sql)
 
-
-
+todos = TodosSQL()
 
 
 
