d91aff7c215c93d675232293b8efb9d12344505d
Filip Gabrys <el.filberto@gmail.com>
2020-07-15 17:13:32 +0200

    Update class TodosSQL

--- todosSQL.py
+++ todosSQL.py
@@ -2,39 +2,41 @@ import json
 import sqlite3
 from sqlite3 import Error
 
-# polączenie z bazą danych
-def create_connection(db_file):
-   conn = None
-   try:
-       conn = sqlite3.connect(db_file)
-       return conn
-   except Error as e:
-       print(e)
 
-   return conn
-
-# stworzenie nowej tabeli
-def execute_sql(conn, sql):
-   try:
-       c = conn.cursor()
-       c.execute(sql)
-   except Error as e:
-       print(e)
 
 
 class TodosSQL:
+    # polączenie z bazą danych
+    def __create_connection(self, db_file):
+        conn = None
+        try:
+            conn = sqlite3.connect(db_file)
+            return conn
+        except Error as e:
+            print(e)
+
+        return conn
+
+# stworzenie nowej tabeli
+    def __execute_sql(self, conn, sql):
+        try:
+            c = conn.cursor()
+            c.execute(sql)
+        except Error as e:
+            print(e)
+
     def __init__(self):
-        conn = create_connection("todo.db")
+        conn = self.__create_connection("todo.db")
         
     def all(self):
-        conn = create_connection("todo.db")
+        conn = self.__create_connection("todo.db")
         cur = conn.cursor()
         cur.execute(f"SELECT * FROM todo")
         rows = cur.fetchall()
         return rows
 
     def get(self, id):
-        conn = create_connection("todo.db")
+        conn = self.__create_connection("todo.db")
         cur = conn.cursor()
         cur.execute(f"SELECT * FROM todo WHERE id={id}")
         rows = cur.fetchall()
@@ -43,7 +45,7 @@ class TodosSQL:
 
     def create(self, data):
          
-        conn = create_connection("todo.db")
+        conn = self.__create_connection("todo.db")
         sql = '''INSERT INTO todo(title, description, done, csrf_token)
                 VALUES(?,?,?,?)'''
         cur = conn.cursor()
@@ -52,7 +54,7 @@ class TodosSQL:
         return cur.lastrowid
 
     def update(self, id, data):
-        conn = create_connection("todo.db")
+        conn = self.__create_connection("todo.db")
         
         values = tuple(v for v in data.values())
         values += (id, )
